52d200c61969cf78a32b8f02cb28389af5bac8a6
ObukhovDA <douxov@mail.ru>
2025-03-06 21:40:34 +0300

    new class n func

--- netcat.py
+++ netcat.py
@@ -6,6 +6,43 @@ import sys
 import textwrap 
 import threading 
 
+class NetCat: 
+    def __init__(self, args, buffer=None): 
+        self.buffer = buffer 
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SOCK_STREAM) 
+
+    def send(self): 
+        self.socket.connect((self.args.target, self.args.port)) 
+        if self.buffer: 
+            self.socket.send(self.buffer) 
+
+        try: 
+            while True: 
+                recv_len = 1 
+                response = '' 
+                while recv_len: 
+                    data = self.socket.recv(4096) 
+                    recv_len = len(data) 
+                    response += data.decode() 
+                    if recv_len < 4096: 
+                        break 
+                    if response: 
+                        print(response) 
+                        buffer = input('> ') 
+                        buffer += '\n' 
+                        self.socket.send(buffer.encode()) 
+        except KeyboardInterrupt: 
+            print('User terminated') 
+            self.socket.close() 
+            sys.exit() 
+
+    def run(self): 
+        if self.args.listen: 
+            self.listen() 
+        else:
+            self.send() 
+
 def execute(cmd): 
     cmd = cmd.strip() 
     if not cmd: 
@@ -43,4 +80,5 @@ if __name__ == '__main__':
 
     nc = NetCat(args, buffer.encode()) 
     nc.run() 
+
 
